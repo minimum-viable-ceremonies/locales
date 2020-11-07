fe961ec2d8660ae8716e1edf6167d7951819714a
James <james.kiesel@gmail.com>
2020-11-07 13:19:30 +1300

    Namespacing for node init

--- node.js
+++ node.js
@@ -2,11 +2,13 @@ const i18n = require('i18next')
 const Backend = require('i18next-fs-backend')
 const { parse } = require('accept-language-parser')
 
-module.exports = (req, file = 'server') => {
+module.exports = (req) => {
   const langs = [... new Set(parse(req.headers['accept-language']).map(lng => lng.code))]
   return i18n.use(Backend).init({
     lng: langs[0],
     fallbackLng: langs.slice(1).concat('en'),
-    backend: { loadPath: `locales/${file}/{{lng}}.yml` }
+    ns: ['server', 'app'],
+    defaultNS: 'server',
+    backend: { loadPath: `locales/{{ns}}/{{lng}}.yml` }
   })
 }
